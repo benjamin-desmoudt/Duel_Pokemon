1180f273470ce27afef5e6fbb8bf9d3fb94c5586
Benjamin Desmoudt <bdesmoudt@hotmail.fr>
2012-11-14 22:09:55 +0100

    projet

--- main.c
+++ main.c
@@ -1,5 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// Vider le buffer
+
+void clean_buffer()
+{
+    int c = 0;
+    while (c != '\n' && c != EOF)
+    {
+        c = getchar();
+    }
+}
+
+/*Cette fonction va permettre d'éviter les "buffer overflow"
+et ainsi empêcher la modification de données extérieures au programme*/
+
+
+int lire(char *chaine, int longueur)
+{
+    char *positionEntree = NULL;
+
+    if (fgets(chaine, longueur, stdin) != NULL)
+    {
+        positionEntree = strchr(chaine, '\n');
+        if (positionEntree != NULL)
+        {
+            *positionEntree = '\0';
+        }
+        else
+        {
+            viderBuffer();
+        }
+        return 1;
+    }
+    else
+    {
+        viderBuffer();
+        return 0;
+    }
+}
 
 
 typedef struct attaque *Attaque;
@@ -58,13 +98,13 @@ void get_pokemon (int i, Pokemon pkmn)
   else
     {
       printf ("%s    %d/%d\n", pkmn->nom , pkmn->hp, pkmn->hp_max);
-      printf ("%s\n%s\n%s\n%s\n", pkmn->t[0]->nom, pkmn->t[1]->nom, pkmn->t[2]->nom, pkmn->t[3]->nom);
+      printf ("1.%s\n2.%s\n3.%s\n4.%s\n", pkmn->t[0]->nom, pkmn->t[1]->nom, pkmn->t[2]->nom, pkmn->t[3]->nom);
     }
 }
 
 
 
-int hit (Attaque atq, Pokemon x, Pokemon y)
+void hit (Attaque atq, Pokemon x, Pokemon y)
 {
   float i = (float)(rand () % 100)/100;
   if (i <= atq->prob )
@@ -80,11 +120,27 @@ int hit (Attaque atq, Pokemon x, Pokemon y)
     }
 }
 
-int good_ans (Pokemon x, Pokemon y, int i)
+void good_ans (Pokemon x, Pokemon y, char* i)
 {
-
+    int atq = atoi(i) - 1;
+    hit (x->t[atq],x, y);
 }
 
+void wrong_ans (Pokemon x, Pokemon y)
+{
+    printf ("\n\n");
+    printf("Reponse incorrecte, veuillez recommencer");
+    char* vraie_atq;
+    fgets (vraie_atq, sizeof(vraie_atq), stdin)
+  if ( strcmp (vraie_atq, "1") || strcmp (vraie_atq, "2") || strcmp (vraie_atq, "3") || strcmp (vraie_atq, "4"))
+  {
+      good_ans(x, y, vraie_atq);
+  }
+  else
+  {
+      return (wrong_ans (x, y));
+  }
+}
 
 
 int combat (Pokemon x, Pokemon y, int i)
@@ -93,18 +149,18 @@ int combat (Pokemon x, Pokemon y, int i)
   get_pokemon (0, y);
   printf("\n---------Votre Pokemon---------\n");
   get_pokemon (1, x);
-  int num_atq;
-  printf("\n Veuillez selectionner une attaque \n")
-  scanf("%d", &num_atq);
-  switch (num_atq)
+  char num_atq[3];
+  printf("\n Veuillez selectionner une attaque \n");
+  fgetc(stdin)
+  if ( )
   {
-      case 1
-      case 2
-      case 3
-      case 4
-      default
+      good_ans(x, y, num_atq);
   }
-  if (y->hp <= 0)
+  else
+  {
+      wrong_ans (x, y);
+  }
+    if (y->hp <= 0)
     {
       return (i % 2);
     }
@@ -193,17 +249,27 @@ Dresseur name_dresseur (char* name)
 
 void combat_final ()
 {
+    //Entrée du nom du 1er dresseur
   printf("Dresseur 1, veuilez entrer votre nom.\n");
-  char name_1[100];
-  scanf ("%s", name_1);
+  char* name_1;
+  fgetc(name_1, sizeof (name_1), stdin);
+  lire ()
   Dresseur d1 = name_dresseur (name_1);
+
+
+  //Ajout des pokemonsdu dresseur 1
   add_pok (new_pokemon ("Pikachu", 50, new_attack("Tonnerre", 25, 1.0), new_attack("Fatal Foudre", 50, 0.0), new_attack("Charge", 50, 0.6), new_attack("Tacle", 50, 0.6)), d1);
   add_pok (new_pokemon ("Raichu", 50, new_attack("Tonnerre", 50, 1.0), new_attack("Fatal Foudre", 50, 0.0), new_attack("Charge", 50, 0.6), new_attack("Tacle", 50, 0.6)), d1);
   get_dresseur (d1);
+
+  //Entrée du nom du deuxième dresseur
   printf("Dresseur 2, veuillez entrer votre nom.\n");
-  char name_2[100];
-  scanf ("%s", name_2);
+  char* name_2;
+  fgets(name_2, sizeof (name_2), stdin);
+  clean_buffer();
   Dresseur d2 = name_dresseur (name_2);
+
+  //Ajout des pokemons du dresseur 2
   add_pok (new_pokemon ("Ratata", 50, new_attack("Tonnerre", 25, 1.0), new_attack("Fatal Foudre", 50, 0.0), new_attack("Charge", 50, 0.6), new_attack("Tacle", 50, 0.6)), d2);
   add_pok (new_pokemon ("Elektek", 50, new_attack("Tonnerre", 50, 1.0), new_attack("Fatal Foudre", 50, 0.0), new_attack("Charge", 50, 0.6), new_attack("Tacle", 50, 0.6)), d2);
   get_dresseur (d2);
